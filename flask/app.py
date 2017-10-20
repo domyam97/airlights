6493abc2ae597246f64ba543e526532ec9bb552a
airlights <airlightsplus@gmail.com>
2017-10-19 20:13:30 -0400

    hi

--- flask/app.py
+++ flask/app.py
@@ -3,14 +3,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/htmlstuff')
+@app.route('/')
 def htmlstuff():
 	return render_template('htmlstuff.html')
 
 
-@app.route('/')
-def index():
-	return "Hello world"
 
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
