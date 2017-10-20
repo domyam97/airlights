fe65448cd1c0013458c8c02cd33b5e5c6c7dcc9f
domyam97 <domyam97@gmail.com>
2017-10-19 20:11:38 -0400

    website

--- Website/app.py
+++ Website/app.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 from send_color import *
 
 app = Flask(__name__)
-
-
+        
+        
 @app.route('/')
 <<<<<<< HEAD:flask/app.py
 def htmlstuff():
@@ -15,9 +15,17 @@ def website():
 	return render_template('index.html')
 >>>>>>> 1609585c474f479c1b71500c0052adf4c0360959:Website/app.py
 
+@app.route("/colors", methods=['POST'])
+def color_in():
+        color = request.form.get("color")
+        pass_color(color)
+        print color
+        return 200
+        
+        
 if __name__ == "__main__":
 	app.run(debug=True, host='0.0.0.0')
 
-@app.route("/colors", methods=['POST'])
-def color_in():
+
+        
 	
--- Website/send_color.py
+++ Website/send_color.py
@@ -1,5 +1,9 @@
 
 
+color = ""
+def pass_color(color_in):
+    color = color_in;
+
 
 
 def send_to_AVR(colors):
