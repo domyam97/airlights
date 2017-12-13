adf8200ae0921731ce375bddda7c2b2c64ec401d
airlights <airlightsplus@gmail.com>
2017-12-12 19:33:44 -0500

    hi

--- I2C/I2C.ino
+++ I2C/I2C.ino
@@ -1,32 +1,44 @@
 #include <Wire.h>
-long gap = 0;
-int totBytes = 0;
+
 void setup() {
   // put your setup code here, to run once:
 
-Serial.begin(9600);
-Wire.begin(0x69);
-delay(1000);
-Serial.println("I2C Slave: "+ 0x69);
-
-Wire.onReceive(handler);
+  Serial.begin(9600);
+  Wire.begin(0x69);
+  delay(1000);
+  Serial.println("I2C Slave: "+ 0x69);
+  
+  Wire.onReceive(handler);
+  
+  pinMode(3, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
 }
+  
 
 void handler(int numBytes){
-  long start = micros();
   int numByte = 0;
+  int* packet = new int[numBytes];
   while(Wire.available())
   {
     int i = Wire.read();
+    if (numByte > 1){packet[numByte-1] = i;}
     numByte ++;  
   }
-  long stp = micros();
-  totBytes += numByte;
-  gap += (stp-start);
-  Serial.println(gap);
-  Serial.println(totBytes);
+  for (int i = 0; i < numBytes-1; i++){
+      Serial.print(i);
+      Serial.print("  ");
+      Serial.println(packet[i]); 
+  }
+  
+  analogWrite(3, packet[3]);
+  analogWrite(5, packet[4]);
+  analogWrite(6, packet[5]);
+  
+  delete[] packet;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
--- Website/send_color.py
+++ Website/send_color.py
@@ -1,18 +1,45 @@
+import colors
+import smbus
 
 
-color = ""
-def pass_color(color_in):
-    color = color_in;
-    ## determine whether color is hex or string // default type is color
-    if color[:1] == '#' :
-        color = '0x' + color[1:]
-        red = int(color[3:4], 16)
-        grn = int(color[5:6], 16)
-        blu = int(color[7:8], 16)
-        print '{0:d}, {1:d}, {2:d}'.format(red, grn, blu)
-        return [red, grn, blu]
-
-
-
-def send_to_AVR(colors):
+def send_to_AVR(color_array):    
+    device_bus = 1
+    addr = 0x69
+    bus = smbus.SMBus(device_bus)
+    bus.write_block_data(addr, 0x00, color_array)
     pass	
+
+def pass_color(color_in):
+	color = color_in;
+	## determine whether color is hex or string // default type is color
+	if color[:1] == '#' :
+		# Decode the hex to integers
+		red = int(color[1:3], 16)
+		green = int(color[3:5], 16)
+		blue = int(color[5:7], 16)
+
+	
+		# Form packet to send single color
+		packet = [55, 0, 0, 2, red, green, blue, 95]
+
+		# Send packet to AVR
+		send_to_AVR(packet)
+
+	else:
+		color = color.lower()
+		if color in colors.colors:
+			hex_col = colors.colors[color]
+			hexColString = str('0x{:06X}'.format(hex_col))[1:]
+
+                        # Decode the hex to integers
+                        red = int(hexColString[1:3], 16)
+                        green = int(hexColString[3:5], 16)
+                        blue = int(hexColString[5:7], 16)
+
+
+                        # Form packet to send single color
+                        packet = [55, 0, 0, 2, red, green, blue, 95]
+
+                        # Send packet to AVR
+                        send_to_AVR(packet)
+
